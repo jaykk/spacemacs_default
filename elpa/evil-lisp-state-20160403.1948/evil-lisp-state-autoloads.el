324c0f349ac079988b4ef8a2c6daf9866dfb8251
qq <qq@localhost.localdomain>
2016-12-11 21:11:42 +1100

    Default install

---
+++ elpa/evil-lisp-state-20160403.1948/evil-lisp-state-autoloads.el
@@ -0,0 +1,16 @@
+;;; evil-lisp-state-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("evil-lisp-state.el") (22605 6445 256990
+;;;;;;  554000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evil-lisp-state-autoloads.el ends here
